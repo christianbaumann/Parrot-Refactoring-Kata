f27060ea6c92b044075317c86fffe7443e5a6f5d
chrissbaumann <mail@christianbaumann.dev>
2023-10-25 08:34:05 +0200

    Removed unused fields

--- Java/src/main/java/parrot/Parrot.java
+++ Java/src/main/java/parrot/Parrot.java
@@ -2,19 +2,9 @@ package parrot;
 
 public class Parrot {
 
-    private final ParrotTypeEnum type;
-    private final int numberOfCoconuts;
-    private final double voltage;
-    private final boolean isNailed;
-
     private ParrotInterface parrot;
 
     public Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
-        this.type = type;
-        this.numberOfCoconuts = numberOfCoconuts;
-        this.voltage = voltage;
-        this.isNailed = isNailed;
-
         parrot = switch (type){
             case EUROPEAN -> new EuropeanParrot(numberOfCoconuts, voltage, isNailed);
             case AFRICAN -> new AfricanParrot(numberOfCoconuts, voltage, isNailed);
